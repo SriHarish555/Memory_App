225ebe284dacb630c2b767659a14f0cfb99bdab1
Sri Harish <sriharish7635@gmail.com>
2023-09-24 23:40:12 +0530

    Memory cards

--- app/src/main/java/com/example/freecodecampmemoryapp/MainActivity.kt
+++ app/src/main/java/com/example/freecodecampmemoryapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.freecodecampmemoryapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvBoard:RecyclerView
+    private lateinit var tvNumMoves:TextView
+    private lateinit var tvNumPair:TextView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvBoard=findViewById(R.id.rvBoard)
+        tvNumMoves=findViewById(R.id.tvNumMoves)
+        tvNumPair=findViewById(R.id.tvNumPairs)
+
+        rvBoard.adapter=MemoryBoardAdaptor(this,8)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager=GridLayoutManager(this,2)
+
     }
 }
---
+++ app/src/main/java/com/example/freecodecampmemoryapp/MemoryBoardAdaptor.kt
@@ -0,0 +1,23 @@
+package com.example.freecodecampmemoryapp
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MemoryBoardAdaptor(private val context: Context,private val numPices: Int) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount()=numPices
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView)
+}
